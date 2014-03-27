6cff097f8578e857c317694d53bdee68ee901e0c
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-03-27 09:22:04 +0100

    fixed issue with address in PD listing

--- ejbs/administrative/src/main/java/org/sola/services/ejb/administrative/repository/entities/SysRegPubDisParcelName.java
+++ ejbs/administrative/src/main/java/org/sola/services/ejb/administrative/repository/entities/SysRegPubDisParcelName.java
@@ -51,10 +51,21 @@ public class SysRegPubDisParcelName extends AbstractReadOnlyEntity {
     @Column(insertable = false, updatable = false, name = "objections")
     @AccessFunctions(onSelect = "administrative.get_objections(name_lastpart)")
     private String objections;
+    @Column(name = "property_location")
+    private String propertyLocation;
     @Column(insertable = false, updatable = false, name = "concatenated_rrr")
     @AccessFunctions(onSelect = "administrative.get_parcel_rrr(ba_unit_id)")
     private String concatenatedRrr;
 
+    public String getPropertyLocation() {
+        return propertyLocation;
+    }
+
+    public void setPropertyLocation(String propertyLocation) {
+        this.propertyLocation = propertyLocation;
+    }
+    
+    
     public String getConcatenatedRrr() {
         return concatenatedRrr;
     }
